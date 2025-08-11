48ccb0e60e2ade432a16cad9c0dc136af0dcc451
kittipong199 <68522978+kittipong199@users.noreply.github.com>
2025-08-12 01:47:03 +0700

    fix: clean coures api comments and imports

--- lib/service/couresApi.dart
+++ lib/service/couresApi.dart
@@ -2,11 +2,8 @@ import 'dart:convert';
 import 'dart:async';
 import 'dart:developer';
 import 'package:app_deaf/models/Coures.dart';
-// import 'package:app_deaf/models/post.dart';
 import 'package:app_deaf/utils/constarts.dart';
 import 'package:http/http.dart' as http;
-
-import 'package:app_deaf/utils/constarts.dart';
 
 class CouresApi {
   static Future<List<Coures>> futureCouresApi() async {
@@ -17,7 +14,7 @@ class CouresApi {
     final response = await http.get(Uri.parse(ursl));
 
     if (response.statusCode == 200) {
-      //pares data
+      // parse data
       final List result = json.decode(response.body);
       return result.map((e) => Coures.fromJson(e)).toList();
     } else {
